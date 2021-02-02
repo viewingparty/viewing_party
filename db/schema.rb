81920d20ce2312e7b74f262332dc4a68f182338d
Max Ribbans <ribbans.max@gmail.com>
2021-02-02 17:14:06 -0500

    Migrate movies and parties
    Co-authored-by: James Fox-Collis <68164897+jlfoxcollis@users.noreply.github.com>
    Co-authored-by: Alex Osborne <64919819+AlexanderOsborne@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_220134) do
+ActiveRecord::Schema.define(version: 2021_02_02_221328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,25 @@ ActiveRecord::Schema.define(version: 2021_02_02_220134) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "duration"
+    t.string "api_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "parties", force: :cascade do |t|
+    t.datetime "time"
+    t.integer "duration"
+    t.bigint "user_id"
+    t.bigint "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_parties_on_movie_id"
+    t.index ["user_id"], name: "index_parties_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +56,6 @@ ActiveRecord::Schema.define(version: 2021_02_02_220134) do
   end
 
   add_foreign_key "friendships", "users"
+  add_foreign_key "parties", "movies"
+  add_foreign_key "parties", "users"
 end
