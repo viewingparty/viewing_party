2d645582858c74b5ac1e8e0fc4e7f794aac4bd77
James Fox-Collis <jlfoxcollis@gmail.com>
2021-02-03 15:41:37 -0700

    Implemented the VCR testing for the_movie_db_api_service_spec
    Co-authored-by: Alex Osborne <64919819+AlexanderOsborne@users.noreply.github.com>
    Co-authored-by: Max Ribbans <52083805+ribbansmax@users.noreply.github.com>

--- spec/services/the_movie_db_api_service_spec.rb
+++ spec/services/the_movie_db_api_service_spec.rb
@@ -1,7 +1,42 @@
 require 'rails_helper'
 
 describe TheMovieDbApiService do
-    it 'can hit the movie endpoints' do
-        
+  it 'can find top rated movies' do
+    VCR.use_cassette("movies_index") do
+      top = TheMovieDbApiService.top_rated_movies
+      expect(top[:results].first[:title]).to eq("Looks Like it Was Yesterday.")
     end
+  end
+
+  it 'can find by title' do
+    VCR.use_cassette("find_by_title") do
+      find = TheMovieDbApiService.find_by_title("Mad Max")
+
+      expect(find[:results].first[:title]).to eq("Mad Max: Fury Road")
+    end
+  end
+
+  it 'can find movie by id' do
+    VCR.use_cassette("find_movie") do
+      movie = TheMovieDbApiService.find_movie(792897)
+
+      expect(movie[:title]).to eq("CBS Playhouse: Shadow Game")
+    end
+  end
+
+  it 'can find_cast' do
+    VCR.use_cassette("find_cast") do
+      cast = TheMovieDbApiService.find_cast(792897)
+
+      expect(cast[:cast].first[:name]).to eq("Daniel Massey")
+    end
+  end
+
+  it 'can find reviews' do
+    VCR.use_cassette("find_reviews") do
+      reviews = TheMovieDbApiService.find_reviews(547016)
+
+      expect(reviews[:results].first[:author]).to eq("MovieGuys")
+    end
+  end
 end
