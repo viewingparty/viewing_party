565f1fae3fc4db511eb2ebb0c96f10c8502d7506
James Fox-Collis <jlfoxcollis@gmail.com>
2021-02-03 00:22:12 -0700

    Added button to movie show page to create a party.
    Co-authored-by: Alex Osborne <64919819+AlexanderOsborne@users.noreply.github.com>
    Co-authored-by: Max Ribbans <52083805+ribbansmax@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   resources :discover, only: [:index]
   resources :movies, only: [:index, :show]
+  resources :parties, only: [:new]
 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -9,5 +9,12 @@ describe 'as an auth user' do
             expect(page).to have_content("#{movie.title}")
             expect(page).to have_content("#{movie.overview}")
         end
+
+        it 'can create a party' do
+            movie = MovieFacade.find_movie(792897)
+            visit movie_path(movie.api_id)
+
+            expect(page).to have_button("Create A Party")
+        end
     end
 end
