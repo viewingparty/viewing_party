04f10e31f47df82e4e3f90630676c74acee62b6b
James Fox-Collis <jlfoxcollis@gmail.com>
2021-02-02 23:17:23 -0700

    discover index page tested

--- Gemfile
+++ Gemfile
@@ -21,9 +21,18 @@ gem 'faraday'
 gem 'figaro'
 
 group :development, :test do
-  gem 'pry'
   gem 'travis'
-  gem 'hirb'
+  gem "pry"
+  gem "rspec-rails", "~> 4.0.1"
+  gem "capybara", github: "DocSpring/capybara", branch: :crash_on_deprecations
+  gem "launchy"
+  gem "simplecov"
+  gem "shoulda-matchers"
+  gem "orderly"
+  gem "rails-erd"
+  gem "awesome_print", :require => "ap"
+  gem "hirb"
+  gem "factory_bot_rails"
 end
 
 group :development do
@@ -34,10 +43,8 @@ group :development do
 end
 
 group :test do
-  gem 'rspec-rails'
-  gem 'capybara'
-  gem 'launchy'
-  gem 'simplecov'
+  gem "webmock"
+  gem "vcr"
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -4,9 +4,9 @@ describe 'When I visit the discover index' do
     it 'can have a link to the top 40 movies' do
         visit discover_index_path
 
-        expect(page).to have_link("Top 40 Movies")
+        expect(page).to have_button("Top Rated Movies")
 
-        click_on "Top 40 Movies"
+        click_on "Top Rated Movies"
 
         expect(current_path).to eq(movies_path)
     end
@@ -14,11 +14,10 @@ describe 'When I visit the discover index' do
     it 'can search for movie titles' do
         visit discover_index_path
 
-        fill_in "search", with: "Mad Max"
+        fill_in :query, with: "Mad Max"
         click_on "Find Movies"
 
         expect(current_path).to eq(movies_path)
         expect(page).to have_content("Mad Max")
-        expect(page).to have_link("Top Rated Movies")
     end
 end
