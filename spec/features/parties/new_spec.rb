2edb386d91eeeefe9537a176a3e8b8e3c7863121
Max Ribbans <ribbans.max@gmail.com>
2021-02-03 18:32:09 -0500

    Failing new party
    Co-authored-by: James Fox-Collis <68164897+jlfoxcollis@users.noreply.github.com>
    Co-authored-by: Alex Osborne <64919819+AlexanderOsborne@users.noreply.github.com>

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -3,10 +3,34 @@ require 'rails_helper'
 RSpec.describe "Making a new Party" do
   describe "It shows movie title and duration" do
     # Fill once API is up and running
+    before(:each) do
+      VCR.insert_cassette("find_movie")
+    end
+
+    after(:each) do
+      VCR.eject_cassette("find_movie")
+    end
+
+    it "Shows uneditable title" do
+      host = User.create(email: 'host@example.com', password: 'hunter2')
+      friend = User.create(email: 'friend@example.com', password: 'hunter2')
+      stranger = User.create(email: 'stranger@example.com', password: 'hunter2')
+
+      visit root_path
+
+      click_button "Log in"
+
+      fill_in :user_email, with: host.email
+      fill_in 'user[password]', with: host.password
+
+      click_button "Log in"
+      visit movie_path("CBS Playhouse: Shadow Game")
+      binding.pry
+    end
   end
 
   describe "Create elements:" do
-    it "date" do
+    xit "date" do
       host = User.create(email: 'host@example.com', password: 'hunter2')
       friend = User.create(email: 'friend@example.com', password: 'hunter2')
       stranger = User.create(email: 'stranger@example.com', password: 'hunter2')
