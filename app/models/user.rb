27d82854805e312ff17808c5efc5d2816a94d15a
Max Ribbans <ribbans.max@gmail.com>
2021-02-02 21:02:23 -0500

    Fix validation to validate email presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :column, :email
+  validates_presence_of :email
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
   has_many :parties, dependent: :destroy
