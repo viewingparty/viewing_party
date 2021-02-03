ab4475679349e61ae679d666238a0e38ab1982c6
James Fox-Collis <jlfoxcollis@gmail.com>
2021-02-03 07:50:06 -0700

    Added testing setup for poros facades and services
    Co-authored-by: Alex Osborne <64919819+AlexanderOsborne@users.noreply.github.com>
    Co-authored-by: Max Ribbans <52083805+ribbansmax@users.noreply.github.com>

---
+++ spec/facades/movie_facade_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe MovieFacade do
+    it 'can return a single movie' do
+        
+    end
+
+end
---
+++ spec/services/the_movie_db_api_service_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe TheMovieDbApiService do
+    it 'can hit the movie endpoints' do
+        
+    end
+end
