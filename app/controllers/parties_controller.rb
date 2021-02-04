b340e7b7f0fd5c5ed338ba9d99868a9be661649d
Max Ribbans <ribbans.max@gmail.com>
2021-02-04 15:20:14 -0500

    Update hidden_field movie_id to be a session[:movie]
    Co-authored-by: James Fox-Collis <68164897+jlfoxcollis@users.noreply.github.com>
    Co-authored-by: Alex Osborne <64919819+AlexanderOsborne@users.noreply.github.com>

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,16 +1,28 @@
 class PartiesController < ActionController::Base
+  before_action :create_movie, only: [:create]
   def new
-    @movie = Movie.create!(title: params[:title], duration: params[:duration], api_id: params[:api_id])
-    @party = Party.new(duration: @movie[:duration])
+    # @movie = Movie.create!(title: params[:title], duration: params[:duration], api_id: params[:api_id])
+    session[:movie] = {title: params[:title],
+      duration: params[:duration],
+      api_id: params[:api_id]
+    }
+    @party = Party.new(duration: params[:duration])
   end
-
+  
   def create
-    current_user.parties.create(party_params)
+    binding.pry
+    current_user.parties.create(party_params.merge(movie: @movie))
     redirect_to user_dashboard_path(current_user)
   end
 
   private
   def party_params
-    params.require(:party).permit(:title, :duration, :movie_id)
+    params.require(:party).permit(:title, :duration)
+  end
+
+  def create_movie
+    if !(@movie = Movie.find_by(title: session[:movie][:title]))
+      @movie = Movie.create(session[:movie])
+    end
   end
 end
