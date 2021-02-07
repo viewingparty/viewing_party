028a4493ba9cc78ac1f7d743c55de0cc39be70d5
awosborn <alexander.w.osborne@gmail.com>
2021-02-07 11:56:45 -0700

    redirect to login if no current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authenticate_user! 
+    redirect_to ‘/login’ unless current_user 
+  end 
 end
