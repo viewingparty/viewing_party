0af556c2d65db2f432746619c910fd351a0461f8
awosborn <alexander.w.osborne@gmail.com>
2021-02-07 12:46:56 -0700

    Update movie_show_spec to add user login

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -6,6 +6,8 @@ describe 'as an auth user' do
         it 'can show me that movies stats' do
           VCR.use_cassette("find_movie") do
             movie = MovieFacade.find_movie(792897)
+            @user = User.create(email: "admin@example.com", password: "password")
+            login_as(@user)
             visit movie_path(movie.api_id)
 
             expect(page).to have_content("#{movie.title}")
@@ -16,6 +18,8 @@ describe 'as an auth user' do
         it 'can create a party' do
           VCR.use_cassette("find_movie") do
             movie = MovieFacade.find_movie(792897)
+            @user = User.create(email: "admin@example.com", password: "password")
+            login_as(@user)
             visit movie_path(movie.api_id)
 
             expect(page).to have_button("Create A Party")
