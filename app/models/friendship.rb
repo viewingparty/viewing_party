1964eda4b36cbd71d01ae9e3ba1b85efff79d7a7
Max Ribbans <ribbans.max@gmail.com>
2021-02-09 18:52:14 -0500

    move approved friends method back to user
    Co-authored-by: Alex Osborne <64919819+AlexanderOsborne@users.noreply.github.com>

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,14 +3,4 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
 
   enum status: %I[pending accepted rejected]
-
-  def self.friends(id)
-    self.approved_friendships(id).map do |friendship|
-      friendship.user_id == id ? friendship.friend : friendship.user
-    end
-  end
-
-  def self.approved_friendships(id)
-    where("friendships.status = 1 AND (friendships.user_id = ? OR friendships.friend_id = ?)", id, id)
-  end
 end
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -10,19 +10,5 @@ RSpec.describe Friendship, type: :model do
   end
 
   describe 'class methods' do
-    it 'can find friends and approved friends' do
-      # for more rigourous testing see "/models/user_spec.rb"
-      user = User.create(email: "admin@example.com", password: "password")
-      user2 = User.create(email: "billy@bob.com", password: "password")
-      user3 = User.create(email: "nope@gmail.com", password: "password")
-      invite = Friendship.create(user: user, friend: user2)
-
-      expect(Friendship.friends(user.id)).to eq([])
-
-      invite.update!(status: 1)
-
-      expect(Friendship.friends(user.id)).to eq([user2])
-      expect(Friendship.friends(user2.id)).to eq([user])
-    end
   end
 end
