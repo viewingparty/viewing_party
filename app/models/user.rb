2352d6c2881f1a1f94cd50cfc94f52e8af7ba4bd
Max Ribbans <ribbans.max@gmail.com>
2021-02-08 20:27:31 -0500

    Refactor for map block

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,17 @@ class User < ApplicationRecord
   validates :email, presence: :email
 
   has_many :friendships, dependent: :destroy
-  # has_many :friends, through: :friendships
   has_many :guests, dependent: :destroy
   has_many :parties, dependent: :destroy
-  # delegate :friends, to: :friendships
 
   def friend_invites
     Friendship.where(friend: self, status: :pending)
   end
 
   def friends
-    (friendships.where(status: 1) + Friendship.where(friend: self).where(status: 1)).map {|friendship| friendship.user_id == self.id ? friendship.friend : friendship.user}
+    approved_friendships = (friendships.where(status: 1) + Friendship.where(friend: self).where(status: 1))
+    approved_friendships.map do |friendship|
+      friendship.user_id == self.id ? friendship.friend : friendship.user
+    end
   end
 end
