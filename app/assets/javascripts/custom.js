f9d97999f83bade1681323e0b07d2c8f007d6149
James Fox-Collis <jlfoxcollis@gmail.com>
2021-02-10 23:33:27 -0700

    Woooo buttons and modals

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,18 @@
 const panels = document.querySelectorAll('.panel');
+const modals = document.querySelectorAll('.modal');
+
+let friendBtn = document.getElementById("friends-btn");
+let friendModal = document.querySelector(".friend-modal");
+let closeFriendBtn = document.querySelector(".close-friends-btn");
+
+let pendingBtn = document.getElementById("pending-btn");
+let pendingModal = document.querySelector(".pending-modal");
+let closePendingBtn = document.querySelector(".close-pending-btn");
+
+let invitesBtn = document.getElementById("invites-btn");
+let invitesModal = document.querySelector(".invites-modal");
+let closeInvitesBtn = document.querySelector(".close-invites-btn");
+// var spans = document.getElementsByClassName("close");
 
 function toggleOpen() {
   console.log('Open!');
@@ -6,3 +20,33 @@ function toggleOpen() {
 }
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+
+friendBtn.onclick = function(){
+  friendModal.style.display = "block"
+}
+closeFriendBtn.onclick = function(){
+  friendModal.style.display = "none"
+}
+
+pendingBtn.onclick = function(){
+  pendingModal.style.display = "block"
+}
+closePendingBtn.onclick = function(){
+  pendingModal.style.display = "none"
+}
+
+invitesBtn.onclick = function(){
+  invitesModal.style.display = "block"
+}
+closeInvitesBtn.onclick = function(){
+  invitesModal.style.display = "none"
+}
+
+
+function closeUpShop(e){
+  if(e.target == e.target){
+    this.style.display = ("none")
+  }
+}
+
+modals.forEach(modal => modal.addEventListener('click', closeUpShop));
